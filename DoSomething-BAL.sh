638a79daea00a62c3ce56d5e237e53121be21258
Jc Yu <jcyu@edge-core.com>
2020-03-03 17:23:07 +0800

    Fix DoSomething-BAL form build ONL to BAL

--- DoSomething-BAL.sh
+++ DoSomething-BAL.sh
@@ -6,7 +6,9 @@
 # IMPORTANT:
 #   When use: './<this script file>  '
 # -------------------------------------------------------
-#./2.0_Build-BAL_ONL.sh
+./1.0_Build-ONL-Prepare.sh
+./1.0_Build-BAL-Prepare.sh
+./2.0_Build-BAL_ONL.sh
 ./2.1_Build-BAL_ASFVOLT16.sh
 ./2.2_Build-BAL_ASGVOLT64.sh
 
